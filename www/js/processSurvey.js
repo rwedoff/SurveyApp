10d04b2a9ea9ce85333a43a4a0e5b609f628ea6d
rwedoff <senor.rw@gmail.com>
2015-07-14 22:55:05 -0500

    Changes

--- www/js/processSurvey.js
+++ www/js/processSurvey.js
@@ -1,5 +1,4 @@
 $(function () {
-    
     $("#thanksSplash").hide();
     $('#newTitle').hide();
     $('#badTitle').hide();
@@ -41,16 +40,15 @@ $(function () {
     $("#newSurveyBut").on('click', function () {
         $('#newTitle').show();
         $('#strtBut').hide();
-         
-        //Creates a date in case new title is NULL
-        var d = new Date(),
-            datetime = "CAB Survey " + (d.getMonth() + 1)  + " " + d.getFullYear() + " " + d.getHours() + " " + d.getMinutes() + " " + d.getSeconds();
         
+         //Creates a date in case new title is NULL
+        var d = new Date(),
+            datetime = "CAB Survey " + (d.getMonth() + 1)  + " " + d.getFullYear() + " " + d.getHours() + " " + d.getMinutes() + " " + d.getSeconds(),
+            $surveyTitle = $("#title").text(),
+            surv = new Survey($surveyTitle, defaultQuestions, 0), //Creates survey object
+            titleList = [];
         $(".surveyTitle").text(datetime);
-        var $surveyTitle = $("#title").text(),
-            surv = new Survey($surveyTitle, defaultQuestions, 0); //Creates survey object
-        var titleList = [];
-        for(var i in localStorage) {
+       for(var i in localStorage) {
             var survey = JSON.parse(localStorage[i]);
             if(survey.title !== undefined) {
                 titleList.push(survey.title);
@@ -59,22 +57,21 @@ $(function () {
         //Save the new Title
         $('#saveTitle').on('click', function () {
             $surveyTitle  = $('#titleIN').val() || datetime;
-            if(titleList.indexOf($surveyTitle) > -1 ){
+            if (titleList.indexOf($surveyTitle) > -1) {
                 $("#badTitle").show();
-            }
-            else {
-            $('#strtBut').show();
-            //Sets the title in the UI
-            $('#newTitle').hide();
-            $('#titleIN').val('');
-            //Creates new survey
-            surv.title = $surveyTitle;
-            $(".surveyTitle").text(surv.title);
-            //Save survey meta in local storage
-            window.localStorage.setItem(surv.title, JSON.stringify(surv));
-            //Update the survey list UI in OLD SURVEY PAGE
-            $("#oldSurveyList").append("<li id='"+surv.title+"'><a href='#newSurvey' id='hrefOnly'>" + surv.title + "</a><span class='ui-li-count'>" + surv.numRes + "</span></li>");
-            $('#oldSurveyList').listview("refresh");
+            } else {
+                $('#strtBut').show();
+                //Sets the title in the UI
+                $('#newTitle').hide();
+                $('#titleIN').val('');
+                //Creates new survey
+                surv.title = $surveyTitle;
+                $(".surveyTitle").text(surv.title);
+                //Save survey meta in local storage
+                window.localStorage.setItem(surv.title, JSON.stringify(surv));
+                //Update the survey list UI in OLD SURVEY PAGE
+                $("#oldSurveyList").append("<li id='" + surv.title + "'><a href='#newSurvey' id='hrefOnly'>" + surv.title + "</a><span class='ui-li-count'>" + surv.numRes + "</span></li>");
+                $('#oldSurveyList').listview("refresh");
             }
         });
     });
@@ -117,6 +114,8 @@ $(function () {
         var $year = $('input[type=radio]:checked', '#year').val() || " ",
             $how = $('input[type=radio]:checked', '#How').val() || " ",
             $else = $('input[type=radio]:checked', '#else').val() || " ",
+            $askEvent = $("input[type=text]", "#askEvent").val() || " ",
+            $askEmail = $("input[type=email]", "#askEmail").val() || " ",
             eventValues = $('input[type="checkbox"]:checked', '#eventType').map(function () {
                 return this.value;
             }).get() || " ",
@@ -134,10 +133,9 @@ $(function () {
             }
             return res;
         }
-        var $askEvent = $("input[type=text]", "#askEvent").val() || " ",
-            $askEmail = $("input[type=email]", "#askEmail").val() || " ";
-        $askEvent = $askEvent.replace(',',' ');
-        $askEmail = $askEmail.replace(',',' ');
+        
+        $askEvent = $askEvent.replace(',', ' ');
+        $askEmail = $askEmail.replace(',', ' ');
     
     
         csvData[0] = "\"" + $year + "\"";
@@ -151,62 +149,150 @@ $(function () {
         return csvData.toString();
     }
     
-   
+
+    
+   //Submit Survey, calls Cordova Read and Write
+    
+    $(".submitSurv").on("click", function () {
+    
+        var thisSurveyTitle = $("#title").text(),
+            thisSurvey =  JSON.parse(window.localStorage.getItem(thisSurveyTitle)),
+            thisSurveyQuestions = thisSurvey.questions,
+            numRes = thisSurvey.numRes;
+         
+        readFile(thisSurvey.title);
+        var processedData = processSurvey();
+        if (processedData !== '" "," ","",""," "," "," "') { //Do not allow empty survey
+            window.console.log(readSurveyData.indexOf(thisSurveyQuestions));
+            if (readSurveyData.indexOf(thisSurveyQuestions) !== -1) {
+                readSurveyData += "\n" + processedData;
+            } else {
+                readSurveyData += thisSurveyQuestions + "\n" + processedData;
+            }
+            
+        }
+        
+        writeFile(thisSurvey.title, readSurveyData);
+        survSplash();
+        thisSurvey.numRes += (1);
+        window.localStorage.setItem(thisSurvey.title, JSON.stringify(thisSurvey));
+        $('#oldSurveyList').listview("refresh");
+        resetForms();
+        activate_subpage("#page_79_6");
+     
+    });
+    //Delete survey, still on file system, but not in app.
+    $('#delBut').on('click', function () {
+        var yesDelete = window.confirm("Are you sure you want to delete this survey? (The CSV file will still be in the file system)");
+        if (yesDelete === true) {
+            var thisSurveyTitle = $("#title").text();
+            window.localStorage.removeItem(thisSurveyTitle);
+            var jqTitle = "#" + thisSurveyTitle;
+            $(jqTitle).remove();
+            $('#oldSurveyList').listview("refresh");
+        }
+    });
+
+    //UI Stuff / Password handling   
+    $("#wrongpass").hide();
+    $("#enterPass").hide();
+    var password = "entertainment";
+    password = localStorage.getItem("password") || "entertainment";
+    
+    
+    $("#lock").on("click", function () {
+        $("#enterPass").toggle();
+    });
+    
+    $("#subPass").on('click', function () {
+        var pass = $("#lockpass").val();
+        if (pass === password) {
+            $("#wrongpass").hide();
+            $("#enterPass").hide();
+            $("#surveySettings2").popup('open');
+            $("#lockpass").val('');
+        } else {
+            $("#lockpass").val('');
+            $("#wrongpass").show();
+        }
+    });
+    
+    //Password Change
+    $("#savePass").on('click', function () {
+         //alert(password);
+        var oldPass = $("#oldPass").val(),
+            newPass = $("#newPass").val(),
+            repPass = $("#repeatPass").val();
+        if (oldPass === password) {
+            if (newPass === "") {
+                window.alert("Password can't be empty");
+            }
+            if (newPass === repPass) {
+                password = newPass;
+                // Store password
+                localStorage.setItem("password", newPass);
+                window.alert("Password has been changed");
+            } else {
+                window.alert("New Password does not match");
+            }
+        } else {
+            window.alert("Wrong Old Password");
+        }
+    });
+
+       
     function readFile(title) {
-      // Wait for device API libraries to load
+         // Wait for device API libraries to load
         
         document.addEventListener("deviceready", onDeviceReady, false);
-
+        function fail(evt) {
+            window.console.log("Reader Error: " + evt.target.error.code);
+        }
+        function gotFS(fileSystem) {
+            fileSystem.root.getFile(title + ".csv", null, gotFileEntry, fail);
+        }
+        function gotFileEntry(fileEntry) {
+            fileEntry.file(gotFile, fail);
+        }
     // device APIs are available
         function onDeviceReady() {
             window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
         }
 
-        function gotFS(fileSystem) {
-            fileSystem.root.getFile(title + ".csv", null, gotFileEntry, fail);
+        function readDataUrl(file) {
+            var reader = new FileReader();
+            reader.onloadend = function(evt) {
+                window.console.log("Read as data URL");
+                window.console.log(evt.target.result);
+                readSurveyUrl = evt.target.result;
+            };
+            reader.readAsDataURL(file);
         }
 
-        function gotFileEntry(fileEntry) {
-            fileEntry.file(gotFile, fail);
+        
+        function readAsText(file) {
+            var reader = new FileReader();
+            reader.onloadend = function(evt) {
+                window.console.log("Read as text");
+             console.log("This is what was read!!!!!!!!!!!!!: " +  evt.target.result);
+                readSurveyData = evt.target.result;
+            
+            };
+            reader.readAsText(file);
         }
 
-        function gotFile(file){
+
+        function gotFile(file) {
             readDataUrl(file);
             readAsText(file);
         
         }
-        
-        function readDataUrl(file) {
-        var reader = new FileReader();
-        reader.onloadend = function(evt) {
-            console.log("Read as data URL");
-            console.log(evt.target.result);
-            readSurveyUrl = evt.target.result;
-        };
-        reader.readAsDataURL(file);
-    }
-
-        function readAsText(file) {
-            var reader = new FileReader();
-            reader.onloadend = function(evt) {
-            console.log("Read as text");
-           // console.log("This is what was read: " +  evt.target.result);
-            readSurveyData = evt.target.result;
             
-            };
-            reader.readAsText(file);
-    
-        }
-
-        function fail(evt) {
-            console.log("Reader Error: " + evt.target.error.code);
-        }
-        
     }
     
     
     
-    function writeFile(title, data){
+    function writeFile(title, data) {
  // Wait for device API libraries to load
     //
         document.addEventListener("deviceready", onDeviceReady, false);
@@ -227,13 +313,13 @@ $(function () {
 
         function gotFileWriter(writer) {
             writer.onwriteend = function(evt) {
-            console.log("contents of file now " + data);
-            writer.truncate(0);
-                writer.onwriteend = function(evt) {
+                window.console.log("contents of file now " + data);
+                writer.truncate(0);
+                writer.onwriteend = function (evt) {
               //      console.log("contents of file now " + data);
               //     writer.seek(4);
                     writer.write(data);
-                    writer.onwriteend = function(evt){
+                    writer.onwriteend = function (evt) {
                 //    console.log("contents of file now " + data);
                     };
                 };
@@ -242,115 +328,14 @@ $(function () {
         }
 
         function fail(error) {
-            console.log("Writer Error: " + error.code);
+            window.console.log("Writer Error: " + error.code);
         }
         
     }
      
     
-   //Submit Survey, calls Cordova Read and Write
-    
-     $(".submitSurv").on("click", function () {
-    
-        var thisSurveyTitle = $("#title").text(),
-            thisSurvey =  JSON.parse(window.localStorage.getItem(thisSurveyTitle)),
-            thisSurveyQuestions = thisSurvey.questions,
-            numRes = thisSurvey.numRes;
-         
-            readFile(thisSurvey.title); 
-         
-        var processedData = processSurvey();
-        if (processedData !== '" "," ","",""," "," "," "') { //Do not allow empty survey
-            console.log(readSurveyData.indexOf(thisSurveyQuestions));
-            if (readSurveyData.indexOf(thisSurveyQuestions) !== -1) {
-                readSurveyData += "\n" + processedData;
-            } else {
-                readSurveyData += thisSurveyQuestions + "\n" + processedData;
-            }
-            
-        }
-        
-        writeFile(thisSurvey.title, readSurveyData);
-        survSplash();
-         thisSurvey.numRes += (1);
-         window.localStorage.setItem(thisSurvey.title, JSON.stringify(thisSurvey));
-         $('#oldSurveyList').listview("refresh");
-        resetForms();
-        activate_subpage("#page_79_6");
-     
-    });
-    //Delete survey, still on file system, but not in app.
-   $('#delBut').on('click', function () {
-       
-       var yesDelete = confirm("Are you sure you want to delete this survey? (The CSV file will still be in the file system)");
-       if(yesDelete == true ) {
-         var thisSurveyTitle = $("#title").text();
-        window.localStorage.removeItem(thisSurveyTitle);
-        var jqTitle = "#" + thisSurveyTitle;
-        $(jqTitle).remove();
-        $('#oldSurveyList').listview("refresh");
-     
-       
-       }
-       
-    });
     
     
-    //UI Stuff / Password handling   
-        $("#wrongpass").hide();
-        $("#enterPass").hide();
-        var password = "entertainment";
-        password = localStorage.getItem("password") || "entertainment";
     
     
-    $("#lock").on("click", function() {
-           $("#enterPass").toggle();
-    });
-    
-    $("#subPass").on('click',function(){
-        var pass = $("#lockpass").val();
-        if (pass === password) {
-            $("#wrongpass").hide();
-            $("#enterPass").hide();
-             $("#surveySettings2").popup('open');           
-            $("#lockpass").val('');
-        }
-        else{
-            $("#lockpass").val('');
-            $("#wrongpass").show();
-        }
-    });
-    
-    
-   
-    
-    //Password Change
-     $("#savePass").on('click',function() {
-         //alert(password);
-       var oldPass = $("#oldPass").val();
-        var newPass = $("#newPass").val();
-         var repPass = $("#repeatPass").val();
-         if(oldPass == password){
-             if(newPass == ""){
-              alert("Password can't be empty");
-             }
-            if(newPass == repPass){
-                password = newPass;
-                // Store password
-                localStorage.setItem("password", newPass);
-                alert("Password has been changed");
-            }
-            else{
-             alert("New Password does not match");   
-            }
-         }
-         else{
-            alert("Wrong Old Password");   
-         }
-    });
-
-    
- 
-   
-        
 });
